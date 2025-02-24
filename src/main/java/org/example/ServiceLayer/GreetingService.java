4e2f37adc956b74df56ebed3aa4333d558f4ec87
Nishank1705 <nishothers@gmail.com>
2025-02-24 11:27:23 +0530

    UC_6 getting all the greetings

---
+++ src/main/java/org/example/ServiceLayer/GreetingService.java
@@ -0,0 +1,38 @@
+package org.example.ServiceLayer;
+import org.example.Repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GreetingService {
+    public static String getGreeting(){
+        return "Hello World";
+    }
+    //  this code is for repository
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public void saveGreeting(String message) {
+        greetingRepository.save(message);
+    }
+
+    public String getGreeting2() {
+        return greetingRepository.find();
+    }
+
+    //  this code is for uc5
+    public void saveGreetingbyid(Long id, String message) {
+        greetingRepository.savebyid(id, message);
+    }
+
+    public String getGreetingById(Long id) {
+        return greetingRepository.findById(id);
+    }
+    //  uc6
+    public Map<Long, String> getAllGreetings() {
+        return greetingRepository.getAllGreetings();
+    }
+}
